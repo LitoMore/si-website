fae5387eeea52db46a06389afbc61afd5dca223d
LitoMore <litomore@gmail.com>
2025-02-21 01:39:22 +0800

    Add icon previews

--- src/components/previewcanvas.tsx
+++ src/components/previewcanvas.tsx
@@ -1,10 +1,50 @@
+// @deno-types="@types/react"
+import { useEffect, useRef, useState } from "react";
+import Konva from "konva";
 import { Image, Layer, Rect, Stage, Text } from "react-konva";
+// @deno-types="../vendor/types/use-image.d.ts"
+import useImage from "use-image";
+import { useIcons } from "#atom";
 import { usePreviewImage } from "#hooks";
 import { getSimpleIconsCdnUrl } from "#utils";
 import { Icon } from "#types";
 
 const PreviewCanvas = ({ icon }: { icon: Icon }) => {
+	const textColor = icon.relativeColor === "#fff" ? "#333" : "#fff";
+	const [icons] = useIcons();
+	const [siSimage] = useImage(
+		`https://cdn.simpleicons.org/simpleicons/${textColor.slice(1)}`,
+	);
 	const [images] = usePreviewImage(getSimpleIconsCdnUrl(icon.slug));
+	const [titleHeight, setTitleHeight] = useState(0);
+	const titleRef = useRef<Konva.default.Text>(null);
+
+	useEffect(() => {
+		setTitleHeight(titleRef.current?.height() ?? 0);
+	}, [icon.title]);
+
+	useEffect(() => {
+		if (siSimage) {
+			siSimage.width = 32;
+			siSimage.height = 32;
+		}
+	}, [siSimage]);
+
+	const textBoxWidth = 300;
+	const baseTextX = 18;
+	const baseTextY = 170;
+	const titleFontSize = 24;
+	const textFontSize = 14;
+	const fontFamily =
+		'-apple-system, BlinkMacSystemFont, "Segoe UI", Roboto, "Helvetica Neue", Arial, "Noto Sans", sans-serif, "Apple Color Emoji", "Segoe UI Emoji", "Segoe UI Symbol", "Noto Color Emoji"';
+
+	const textProps = {
+		fill: textColor,
+		fontSize: textFontSize,
+		fontFamily,
+		fontStyle: "300",
+		letterSpacing: 0,
+	};
 
 	return (
 		<Stage width={720} height={400}>
@@ -15,7 +55,39 @@ const PreviewCanvas = ({ icon }: { icon: Icon }) => {
 					fill={icon.relativeColor}
 					cornerRadius={5}
 				/>
-				<Text text={icon.title} y={200} fontSize={24} />
+				<Text
+					ref={titleRef}
+					{...textProps}
+					fontSize={titleFontSize}
+					width={textBoxWidth}
+					x={baseTextX}
+					y={baseTextY}
+					text={icon.title}
+				/>
+				<Text
+					{...textProps}
+					width={textBoxWidth}
+					x={baseTextX}
+					y={baseTextY + titleHeight + 2}
+					lineHeight={1.25}
+					text={[
+						`${icon.slug}.svg`,
+						"",
+						`Brand: ${icon.title}`,
+						`Color: #${icon.hex}`,
+					].join("\n")}
+				/>
+				<Image x={baseTextX - 4} y={330} image={siSimage} />
+				<Text
+					{...textProps}
+					x={baseTextX + 36}
+					y={330}
+					lineHeight={1.25}
+					text={[
+						`${icons.data.length} SVG brand icons`,
+						"available at simpleicons.org",
+					].join("\n")}
+				/>
 				{images.map((image) => {
 					if (!image) return null;
 					return (
