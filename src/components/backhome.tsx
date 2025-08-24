20a1a086b6703f71fc73ee53b9062a308797a895
LitoMore <litomore@gmail.com>
2025-08-24 17:13:56 +0800

    Fix back icon color

--- src/components/backhome.tsx
+++ src/components/backhome.tsx
@@ -1,13 +1,16 @@
 import {IconArrowLeft} from '@tabler/icons-react';
 import {useNavigate} from 'react-router';
+import {useColorScheme} from '#hooks';
 
 export default function BackHome() {
 	const navigate = useNavigate();
+	const {iconFg} = useColorScheme();
 
 	return (
 		<div className="fixed top-0 left-0 p-2.5">
 			<IconArrowLeft
 				className="control"
+				style={{color: iconFg}}
 				onClick={() => {
 					void navigate('/');
 				}}
